d19c35107784ddad38410bc9b060c88d680aaaf9
Matthew Collett <matthew.collett12@gmail.com>
2023-03-27 22:06:05 -0300

    Update config.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x build.sh
 docker build -t $DOCKER_CONTAINER_NAME .
 docker tag $DOCKER_CONTAINER_NAME:latest $ECR/$DOCKER_CONTAINER_NAME:latest
 docker push $ECR/$DOCKER_CONTAINER_NAME:latest
