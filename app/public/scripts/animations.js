74299ec2f39ca519c27e55717f372bba6e7b60b8
Matthew Collett <matthew.collett12@gmail.com>
2023-03-29 21:53:42 -0300

    Format and start rates page

--- app/public/scripts/animations.js
+++ app/public/scripts/animations.js
@@ -1,3 +1,3 @@
 ScrollTrigger.batch('.img-anim', {
-    onEnter: batch => gsap.from(batch, {autoAlpha: 0, duration: 2, stagger: 0.1, ease: 'power3', overwrite: true})
+    onEnter: batch => gsap.from(batch, { autoAlpha: 0, duration: 2, stagger: 0.1, ease: 'power3', overwrite: true })
 });
