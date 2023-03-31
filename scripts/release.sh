3c7fc98eb61e5e61f7ba7fd292f5ae773cba9a38
Matthew Collett <matthew.collett12@gmail.com>
2023-03-30 22:15:45 -0300

    Update config.yml

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #release.sh
-export RELEASE_NOTES="$(gren release --tags=$VERSION --data-source=commits)"
-github-release release \
-    --user "$CIRCLE_USERNAME" \
-    --repo "$CIRCLE_PROJECT_REPONAME" \
-    --tag "$VERSION" \
-    --name "$CIRCLE_PROJECT_REPONAME $VERSION" \
+RELEASE_NOTES="$(gren release --tags=$VERSION --data-source=commits)"
+zip -r $CIRCLE_ARTIFACTS/$CIRCLE_PROJECT_REPONAME-$VERSION.zip dist/*
+git config user.name $CIRCLE_USERNAME
+gren --override --data-source=commits
+github-release upload \
+    --user $CIRCLE_USERNAME \
+    --repo $CIRCLE_PROJECT_REPONAME \
+    --tag $VERSION \
+    --name $CIRCLE_PROJECT_REPONAME"."$VERSION"-build-"$VERSION".zip" \
+    --file $CIRCLE_ARTIFACTS/$CIRCLE_PROJECT_REPONAME-$VERSION.zip \
     --description "$RELEASE_NOTES"
