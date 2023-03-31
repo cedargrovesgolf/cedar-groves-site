fba77efb8635f3abea221f647d358eef48175a18
Matthew Collett <matthew.collett12@gmail.com>
2023-03-30 23:15:48 -0300

    Update config.yml

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #release.sh
-github-release upload \
+github-release release \
     --user $CIRCLE_USERNAME \
     --repo $CIRCLE_PROJECT_REPONAME \
     --tag $VERSION \
     --name $CIRCLE_PROJECT_REPONAME"."$VERSION"-build-"$VERSION".zip" \
-    --file $CIRCLE_ARTIFACTS/$CIRCLE_PROJECT_REPONAME-$VERSION.zip \
+    --description "test"
