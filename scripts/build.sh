7396e7d6d295bc7096089fadfd5bb6ca2a569586
Matthew Collett <matthew.collett12@gmail.com>
2023-03-30 21:43:41 -0300

    Update config.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #build.sh
-docker build -t $CIRCLE_PROJECT_REPONAME .
-docker tag $CIRCLE_PROJECT_REPONAME:latest $ECR/$CIRCLE_PROJECT_REPONAME:latest
+docker build -t $ECR/$CIRCLE_PROJECT_REPONAME:$VERSION -t $ECR/$CIRCLE_PROJECT_REPONAME:latest .
+docker push $ECR/$CIRCLE_PROJECT_REPONAME:$VERSION
 docker push $ECR/$CIRCLE_PROJECT_REPONAME:latest
