a8bf8fcd2a1727e1b466879262270030af45f560
Matthew Collett <matthew.collett12@gmail.com>
2023-03-31 00:02:39 -0300

    Update config.yml

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #release.sh
-github-release -v release \
+github-release release \
     --user $CIRCLE_USERNAME \
     --repo $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME \
     --tag "v1.0.0" \
-    --security-token $GITHUB_TOKEN
+    --security-token $GITHUB_TOKEN \
