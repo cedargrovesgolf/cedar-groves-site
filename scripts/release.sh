044207b749156f7dac3d4d43afd638cde50b7138
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2023-03-31 21:05:14 -0300

    touch up

--- scripts/release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#release.sh
-github-release release \
-    --user $CIRCLE_USERNAME \
-    --repo $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME \
-    --tag "v1.0.0" \
-    --security-token $GITHUB_TOKEN \
