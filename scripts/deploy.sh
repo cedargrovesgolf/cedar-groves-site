d3b202362b8d6826155843a263114bcb48bdca0a
matthew-collett <97645707+matthew-collett@users.noreply.github.com>
2023-04-02 03:55:11 -0300

    rates & fees, aerial tour, update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 ssh-keyscan $EC2_INSTANCE_IP_ADDRESS >> ~/.ssh/known_hosts
 ssh $EC2_INSTANCE_USERNAME@$EC2_INSTANCE_IP_ADDRESS "\
   aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR && \
+  docker image prune -af \
   docker pull $ECR/$CIRCLE_PROJECT_REPONAME:latest && \
   if docker ps -a | grep $CIRCLE_PROJECT_REPONAME; then \
     docker stop $CIRCLE_PROJECT_REPONAME && \
