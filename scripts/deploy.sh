7bbce45395a726139b0917eee1f54d28bd53dd9c
Matthew Collett <matthew.collett12@gmail.com>
2023-04-13 23:32:24 -0300

    fix: deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,17 @@
 ssh-keyscan $EC2_INSTANCE_IP_ADDRESS >> ~/.ssh/known_hosts
 ssh $EC2_INSTANCE_USERNAME@$EC2_INSTANCE_IP_ADDRESS "\
   aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR && \
+  docker system prune -af && \
+  docker container prune -f && \
   docker image prune -af && \
+  docker network prune -f && \
+  docker volume prune -f && \
+  echo $SUDO | sudo -S systemctl stop docker && \
+  echo $SUDO | sudo -S rm -rf /var/lib/docker/* && \
+  echo $SUDO | sudo -S systemctl start docker && \
   docker pull $ECR/$CIRCLE_PROJECT_REPONAME:latest && \
   if docker ps -a | grep $CIRCLE_PROJECT_REPONAME; then \
     docker stop $CIRCLE_PROJECT_REPONAME && \
     docker rm $CIRCLE_PROJECT_REPONAME; \
   fi && \
-  docker run -v ~/.aws:/root/.aws -d -p $PORT:$PORT --name $CIRCLE_PROJECT_REPONAME $ECR/$CIRCLE_PROJECT_REPONAME:latest"
+  docker run -d -p $PORT:$PORT --name $CIRCLE_PROJECT_REPONAME $ECR/$CIRCLE_PROJECT_REPONAME:latest" #-v ~/.aws:/root/.aws
