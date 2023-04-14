c86a16012b257a1c56228822addedacebeb3f74f
Matthew Collett <matthew.collett12@gmail.com>
2023-04-14 00:00:38 -0300

    fix: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,15 @@
 #deploy.sh
 ssh-keyscan $EC2_INSTANCE_IP_ADDRESS >> ~/.ssh/known_hosts
 ssh $EC2_INSTANCE_USERNAME@$EC2_INSTANCE_IP_ADDRESS "\
-  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR && \
-  docker system prune -af && \
-  docker container prune -f && \
-  docker image prune -af && \
-  docker network prune -f && \
-  docker volume prune -f && \
-  echo $SUDO | sudo -S systemctl stop docker && \
-  echo $SUDO | sudo -S rm -rf /var/lib/docker/* && \
-  echo $SUDO | sudo -S systemctl start docker && \
-  docker pull $ECR/$CIRCLE_PROJECT_REPONAME:latest && \
+  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR; \
+  docker system prune -af; \
+  docker container prune -f; \
+  docker image prune -af; \
+  docker network prune -f; \
+  docker volume prune -f; \
+  docker pull $ECR/$CIRCLE_PROJECT_REPONAME:latest; \
   if docker ps -a | grep $CIRCLE_PROJECT_REPONAME; then \
-    docker stop $CIRCLE_PROJECT_REPONAME && \
+    docker stop $CIRCLE_PROJECT_REPONAME; \
     docker rm $CIRCLE_PROJECT_REPONAME; \
-  fi && \
+  fi; \
   docker run -e PORT=$PORT -d -v ~/.aws:/root/.aws -p $PORT:$PORT --name $CIRCLE_PROJECT_REPONAME $ECR/$CIRCLE_PROJECT_REPONAME:latest"
