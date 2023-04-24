683691bd147c2f400da3232633e7108f677d671f
Matthew Collett <matthew.collett12@gmail.com>
2023-04-23 22:36:16 -0300

    fix: header, optimize, favicon

--- app/routes/index.js
+++ app/routes/index.js
@@ -25,12 +25,75 @@ router.get('/', async (req, res) => {
   const openHour = await getItem({ ...baseQuery, keyValue: 'open' });
   const closeHour = await getItem({ ...baseQuery, keyValue: 'close' });
 
+  const reviews = [
+    {
+      review:
+        'I love this course, the people who work there are friendly and the course is great. I love the lay out and it has helped my my chip game a ton.',
+      author: 'Allan Shephard',
+      stars: 5
+    },
+    {
+      review:
+        'By far the most executive par 3 I have ever played. Best greens in the city and the tee boxes were very well maintained. I took my boss for a quick 9 after work and got a raise out of it.',
+      author: 'Carson Dorcas',
+      stars: 5
+    },
+    {
+      review:
+        'Awesome place. Great staff. Ample tee areas. Great putting green. The only thing you really need is ice cream!',
+      author: 'D Conzie Connell',
+      stars: 5
+    },
+    {
+      review:
+        "A nice, challenging par 3 course. Greens are in good shape for how little rain we've had this year. Tee boxes have taken a beating, but is a lot of fun for the price!",
+      author: 'Matthew Chase',
+      stars: 4
+    },
+    {
+      review: 'A great par 3 course, we are here every week.',
+      author: 'Robert Brennan',
+      stars: 5
+    },
+    {
+      review:
+        'More challenging than I anticipated, which is great. I also loved that there was a recycling bin at each hole. *Patrons... please be courteous and use them!*',
+      author: 'Denise Hutton',
+      stars: 5
+    },
+    {
+      review:
+        'A great place to spend some time practicing on the driving range or the putting green. Very reasonably priced and the staff was friendly.',
+      author: 'Brent',
+      stars: 4
+    },
+    {
+      review:
+        'This is a great par 3 course with the chance to open up with your driver a few times',
+      author: 'Mike Davidson',
+      stars: 5
+    },
+    {
+      review:
+        'Laid out well, staff is friendly. May be an executive par 3, but it has its challenges. This course will test your irons.',
+      author: 'Joe Cruise',
+      stars: 5
+    },
+    {
+      review:
+        'Great 9 hole course with some excellent hazards to make it challenging.',
+      author: 'Mike Goddard',
+      stars: 4
+    }
+  ];
+
   var body = {
     heroTitle: 'Cedar Groves Executive<br>Par 3 & Driving Range',
     heroSubtitle: 'Golf Course & Driving Range',
     heroImg: 'assets/images/drone-1.jpg',
     openHour: get12Hr(openHour.hour.S),
-    closeHour: get12Hr(closeHour.hour.S)
+    closeHour: get12Hr(closeHour.hour.S),
+    reviews: reviews
   };
   // render the page
   res.render('index', body);
--- app/routes/rates-fees.js
+++ app/routes/rates-fees.js
@@ -3,10 +3,92 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  const ratesFees = [
+    {
+      title: 'The Course',
+      image: 9,
+      rates: [
+        {
+          desc: '9 Hole',
+          price: '$25'
+        },
+        {
+          desc: '9 Hole - Senior',
+          price: '$20'
+        },
+        {
+          desc: '9 Hole - Junior',
+          price: '$15'
+        },
+        {
+          desc: '18 Hole',
+          price: '$36'
+        },
+        {
+          desc: '18 Hole - Senior',
+          price: '$28'
+        },
+        {
+          desc: '18 Hole - Junior',
+          price: '$23'
+        }
+      ]
+    },
+    {
+      title: 'Driving Range',
+      image: 15,
+      rates: [
+        {
+          desc: 'S Bucket - 25 Balls',
+          price: '$3.50'
+        },
+        {
+          desc: 'M Bucket - 50 Balls',
+          price: '$7'
+        },
+        {
+          desc: 'L Bucket - 75 Balls',
+          price: '$10.50'
+        },
+        {
+          desc: 'XL Bucket - 100 Balls',
+          price: '$14'
+        },
+        {
+          desc: 'Grass Tee Box',
+          price: '$3'
+        }
+      ]
+    },
+    {
+      title: 'Amenities',
+      image: 6,
+      rates: [
+        {
+          desc: 'Rental Set',
+          price: '$12'
+        },
+        {
+          desc: 'Pull Cart',
+          price: '$5'
+        },
+        {
+          desc: 'Recycled Balls (6 Pack)',
+          price: '$5'
+        },
+        {
+          desc: 'Tees (15 Pack)',
+          price: '$1'
+        }
+      ]
+    }
+  ];
+
   var body = {
     heroTitle: 'Rates & Fees',
     heroSubtitle: 'Affordable Golf for Every Player',
-    heroImg: 'assets/images/drone-2.jpg'
+    heroImg: 'assets/images/drone-2.jpg',
+    ratesFees: ratesFees
   };
   // render the page
   res.render('rates-fees', body);
